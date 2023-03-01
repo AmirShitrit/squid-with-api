ed9c1ae72d318729a174bbcf05462e086877ca27
Amir Shitrit <amirchip@gmail.com>
2023-03-01 13:20:56 +0200

    w/concurrent requests

--- server/server_integration_test.go
+++ server/server_integration_test.go
@@ -1,45 +1,64 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 
+	"github.com/samber/lo"
 	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 )
 
 type InMemoryProxiesStore struct {
 	proxies map[string]ProxyUrl
+	lock    sync.Mutex
 }
 
 func (i *InMemoryProxiesStore) GetProxyDetails(host string) (ProxyUrl, bool) {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	proxy, ok := i.proxies[host]
 	return proxy, ok
 }
 
 func (i *InMemoryProxiesStore) GetAll() []ProxyUrl {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	return maps.Values(i.proxies)
 }
 
 func (i *InMemoryProxiesStore) SetProxy(host string, proxy ProxyUrl) error {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 	i.proxies[host] = proxy
 	return nil
 }
 
 func TestRegisteringProxiesAndRetrievingThem(t *testing.T) {
-	store := &InMemoryProxiesStore{make(map[string]ProxyUrl)}
+	store := &InMemoryProxiesStore{make(map[string]ProxyUrl), sync.Mutex{}}
 	server := NewConfigServer(store)
 
-	wantedProxies := []string{
-		"http://user1:pwd1@proxy1:1001",
-		"http://user2:pwd2@proxy2:1002",
-		"http://user3:pwd3@proxy3:1003",
-	}
+	wantedProxies := lo.Map(
+		lo.RangeFrom(0, 50),
+		func(item int, _ int) string {
+			padded := fmt.Sprintf("1%03d", item)
+			return fmt.Sprintf("http://user%[1]s:pwd%[1]s@proxy%[1]s:1%[1]s", padded)
+		})
+
+	w := sync.WaitGroup{}
 	for _, proxy := range wantedProxies {
-		server.ServeHTTP(httptest.NewRecorder(), newPostProxyRequest(proxy))
+		w.Add(1)
+		proxy := proxy
+		go func() {
+			server.ServeHTTP(httptest.NewRecorder(), newPostProxyRequest(proxy))
+			w.Done()
+		}()
 	}
+	w.Wait()
 
 	response := httptest.NewRecorder()
 	server.ServeHTTP(response, newGetAllRequest())
